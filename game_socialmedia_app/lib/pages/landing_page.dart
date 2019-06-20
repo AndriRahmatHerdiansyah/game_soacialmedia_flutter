c8a2c74bd597e5fb44003b05df4e327d6b5ad1bb
Andri Rahmat Herdiansyah <andrirahmatherdiansyah@Andris-MacBook-Pro.local>
2019-06-20 11:06:06 +0700

    Create search field

--- game_socialmedia_app/lib/pages/landing_page.dart
+++ game_socialmedia_app/lib/pages/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:game_socialmedia_app/common/app_background.dart';
 import 'package:game_socialmedia_app/common/list_view_game.dart';
+import 'package:game_socialmedia_app/styleguide/colors.dart';
 import 'package:game_socialmedia_app/styleguide/text_styles.dart';
 // import 'package:game_socialmedia_app/styleguide/colors.dart';
 
@@ -8,7 +9,8 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
+
       body: Stack(
         children: <Widget>[
           AppBackground(),
@@ -29,10 +31,31 @@ class LandingPage extends StatelessWidget {
                   style: wellconeTextStyle
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 16.0),
                   child: Text(
                     'Explore most popular game in the world',
                     style: headingStyle,
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  child: TextField(
+                    cursorColor: primaryColor,
+                    textInputAction: TextInputAction.search,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 14.0),
+                      hintText: 'Search for the game...',
+                      border: InputBorder.none,
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Material(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          color: primaryColor,
+                          child: Icon(Icons.search, color: Colors.white),
+                        ),
+                      )
+                    ),
                   ),
                 )
               ],
